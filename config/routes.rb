02ec3d1e7004e644c7926df625f56abd09b23e66
HM2468 <huangmiao2468@gmail.com>
2020-04-13 23:40:23 +0100

    Modified route file

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   get 'user/login_page'
   post 'user/login'
   get 'user/logout'
+  get 'user/center'
+  post "user/update_basic"
+  post "user/change_pwd"
   get 'cart/add2cart'
   get 'cart/cart_page'
+  post 'cart/delete_goods'
   get 'order/order_page'
   post 'order/check'
+  post 'order/delete'
   get "home/main"
   get "home/records"
   root "home#main"
